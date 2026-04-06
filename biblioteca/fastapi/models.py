d1443ad4b203ffea14954c10ed36cccd10738636
natal <natalia.blanco.t@gmail.com>
2026-04-06 13:19:59 +0200

    creación de models

---
+++ biblioteca/fastapi/models.py
@@ -0,0 +1,9 @@
+import sqlalchemy import Column, Integer, String, Boolean
+from database import Base
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    titulo = Column(String, nullable=False)
+    autor = Column(String, nullable=False)
+    genero = Column(String, nullable=False)
+    disponible = Column(Boolean, nullable=False)
