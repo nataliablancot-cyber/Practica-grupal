6fa6fee7ae2e47c640d22cb46b2063e48ab76063
natal <natalia.blanco.t@gmail.com>
2026-04-24 09:21:10 +0200

    cambios models

--- biblioteca/fastapi/models.py
+++ biblioteca/fastapi/models.py
@@ -8,3 +8,12 @@ class Book(Base):
     genero = Column(String, nullable=False)
     disponible = Column(Boolean, nullable=False)
 
+class Loan(Base):
+    __tablename__ = "loans"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    usuario_id = Column(String, nullable=False)
+    libro_id = Column(Integer, nullable=False)
+    fecha_prestamo = Column(Date, default=datetime.date.today)
+    fecha_devolucion = Column(Date, nullable=True)
+    estado = Column(String, default="Activo")
+
